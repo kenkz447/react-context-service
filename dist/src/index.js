3af993c426d1df60e39c188923aa4ab2285e9c0c
kenkz447 <kenkz447@gmail.com>
2019-04-04 18:00:01 +0700

    change build target to es5

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,4 +1,28 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __rest = (this && this.__rest) || function (s, e) {
     var t = {};
     for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
@@ -8,6 +32,16 @@ var __rest = (this && this.__rest) || function (s, e) {
             t[p[i]] = s[p[i]];
     return t;
 };
+var __values = (this && this.__values) || function (o) {
+    var m = typeof Symbol === "function" && o[Symbol.iterator], i = 0;
+    if (m) return m.call(o);
+    return {
+        next: function () {
+            if (o && i >= o.length) o = void 0;
+            return { value: o && o[i++], done: !o };
+        }
+    };
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -16,76 +50,107 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const React = __importStar(require("react"));
-class ContextCreator extends React.Component {
-    constructor(props) {
-        super(props);
-        const { context, initContextValue } = this.props;
-        this.Context = context || React.createContext(initContextValue);
-        ContextCreator.instance = this;
+var React = __importStar(require("react"));
+var ContextCreator = /** @class */ (function (_super) {
+    __extends(ContextCreator, _super);
+    function ContextCreator(props) {
+        var _this = _super.call(this, props) || this;
+        var _a = _this.props, context = _a.context, initContextValue = _a.initContextValue;
+        _this.Context = context || React.createContext(initContextValue);
+        ContextCreator.instance = _this;
+        return _this;
     }
-    render() {
-        const { loggingEnabled, children, initContextValue } = this.props;
-        return (React.createElement(Provider, { ref: (e) => this.provider = e, initContextValue: initContextValue, loggingEnabled: loggingEnabled }, children));
-    }
-}
+    ContextCreator.prototype.render = function () {
+        var _this = this;
+        var _a = this.props, loggingEnabled = _a.loggingEnabled, children = _a.children, initContextValue = _a.initContextValue;
+        return (React.createElement(Provider, { ref: function (e) { return _this.provider = e; }, initContextValue: initContextValue, loggingEnabled: loggingEnabled }, children));
+    };
+    return ContextCreator;
+}(React.Component));
 exports.ContextCreator = ContextCreator;
-class Provider extends React.Component {
-    constructor(props) {
-        super(props);
-        this.setContextProxy = (source, newContext) => {
-            const { loggingEnabled } = this.props;
-            const newContextKey = Object.keys(newContext);
-            const oldContext = this.getContext(newContextKey);
-            const setContextCallback = (() => {
+var Provider = /** @class */ (function (_super) {
+    __extends(Provider, _super);
+    function Provider(props) {
+        var _this = _super.call(this, props) || this;
+        _this.setContextProxy = function (source, newContext) {
+            var loggingEnabled = _this.props.loggingEnabled;
+            var newContextKey = Object.keys(newContext);
+            var oldContext = _this.getContext(newContextKey);
+            var setContextCallback = (function () {
                 if (loggingEnabled) {
-                    this.log(source, newContext, oldContext);
+                    _this.log(source, newContext, oldContext);
                 }
             });
-            this.setState(newContext, setContextCallback);
+            _this.setState(newContext, setContextCallback);
         };
-        this.getContext = (...contextKeys) => {
-            if (!contextKeys || !contextKeys.length) {
-                return Object.seal(this.state);
+        _this.getContext = function () {
+            var contextKeys = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                contextKeys[_i] = arguments[_i];
             }
-            return contextKeys.reduce((gettedContext, currentKey) => {
-                gettedContext[currentKey] = this.state[currentKey];
+            if (!contextKeys || !contextKeys.length) {
+                return Object.seal(_this.state);
+            }
+            return contextKeys.reduce(function (gettedContext, currentKey) {
+                gettedContext[currentKey] = _this.state[currentKey];
                 return gettedContext;
             }, {});
         };
-        this.log = (source, newContext, oldContext) => {
+        _this.log = function (source, newContext, oldContext) {
             console.group('Context was changed');
             console.log('By: ', source);
             console.log('From:', oldContext);
             console.log('To:', newContext);
             console.groupEnd();
         };
-        const { initContextValue } = props;
-        const { setContextProxy, getContext } = this;
-        this.state = Object.assign({}, initContextValue, { setContext(context) {
+        var initContextValue = props.initContextValue;
+        var _a = _this, setContextProxy = _a.setContextProxy, getContext = _a.getContext;
+        _this.state = __assign({}, initContextValue, { setContext: function (context) {
                 setContextProxy(this, context);
             }, getContext: getContext });
+        return _this;
     }
-    render() {
-        const { Context } = ContextCreator.instance;
+    Provider.prototype.render = function () {
+        var Context = ContextCreator.instance.Context;
         return (React.createElement(Context.Provider, { value: this.state }, this.props.children));
-    }
-}
+    };
+    return Provider;
+}(React.Component));
 exports.Provider = Provider;
-class InjectedWrapper extends React.PureComponent {
-    render() {
-        const _a = this.props, { Component } = _a, props = __rest(_a, ["Component"]);
-        return (React.createElement(Component, Object.assign({}, props)));
+var InjectedWrapper = /** @class */ (function (_super) {
+    __extends(InjectedWrapper, _super);
+    function InjectedWrapper() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-}
-function withContext(...keys) {
+    InjectedWrapper.prototype.render = function () {
+        var _a = this.props, Component = _a.Component, props = __rest(_a, ["Component"]);
+        return (React.createElement(Component, __assign({}, props)));
+    };
+    return InjectedWrapper;
+}(React.PureComponent));
+function withContext() {
+    var keys = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        keys[_i] = arguments[_i];
+    }
     return function (Component) {
-        const getContextToProps = (context) => {
-            const contextToProps = {};
+        var getContextToProps = function (context) {
+            var e_1, _a;
+            var contextToProps = {};
             if (keys) {
-                // Add context request by keys
-                for (const contextKey of keys) {
-                    contextToProps[contextKey] = context[contextKey];
+                try {
+                    // Add context request by keys
+                    for (var keys_1 = __values(keys), keys_1_1 = keys_1.next(); !keys_1_1.done; keys_1_1 = keys_1.next()) {
+                        var contextKey = keys_1_1.value;
+                        contextToProps[contextKey] = context[contextKey];
+                    }
+                }
+                catch (e_1_1) { e_1 = { error: e_1_1 }; }
+                finally {
+                    try {
+                        if (keys_1_1 && !keys_1_1.done && (_a = keys_1.return)) _a.call(keys_1);
+                    }
+                    finally { if (e_1) throw e_1.error; }
                 }
             }
             // Add required context
@@ -93,40 +158,47 @@ function withContext(...keys) {
             contextToProps.getContext = context.getContext;
             return contextToProps;
         };
-        return class Injector extends React.PureComponent {
-            constructor() {
-                super(...arguments);
-                this.renderConsumer = (context) => {
-                    const contextToProps = getContextToProps(context);
-                    const componentPropsWithContext = Object.assign(contextToProps, this.props);
-                    return React.createElement(InjectedWrapper, Object.assign({ Component: Component }, componentPropsWithContext));
+        return /** @class */ (function (_super) {
+            __extends(Injector, _super);
+            function Injector() {
+                var _this = _super !== null && _super.apply(this, arguments) || this;
+                _this.renderConsumer = function (context) {
+                    var contextToProps = getContextToProps(context);
+                    var componentPropsWithContext = Object.assign(contextToProps, _this.props);
+                    return React.createElement(InjectedWrapper, __assign({ Component: Component }, componentPropsWithContext));
                 };
+                return _this;
             }
-            render() {
-                const { Context } = ContextCreator.instance;
+            Injector.prototype.render = function () {
+                var Context = ContextCreator.instance.Context;
                 return (React.createElement(Context.Consumer, null, this.renderConsumer));
-            }
-        };
+            };
+            return Injector;
+        }(React.PureComponent));
     };
 }
 exports.withContext = withContext;
-class ContextRender extends React.PureComponent {
-    constructor() {
-        super(...arguments);
-        this.renderConsumer = (context) => {
-            const { children, keys } = this.props;
-            const contextToProps = keys.reduce((childContext, childContextKey) => {
-                return Object.assign({}, childContext, { [childContextKey]: context[childContextKey] });
+var ContextRender = /** @class */ (function (_super) {
+    __extends(ContextRender, _super);
+    function ContextRender() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.renderConsumer = function (context) {
+            var _a = _this.props, children = _a.children, keys = _a.keys;
+            var contextToProps = keys.reduce(function (childContext, childContextKey) {
+                var _a;
+                return __assign({}, childContext, (_a = {}, _a[childContextKey] = context[childContextKey], _a));
             }, {
                 setContext: context.setContext,
                 getContext: context.getContext
             });
             return children(contextToProps);
         };
+        return _this;
     }
-    render() {
-        const { Context } = ContextCreator.instance;
+    ContextRender.prototype.render = function () {
+        var Context = ContextCreator.instance.Context;
         return (React.createElement(Context.Consumer, null, this.renderConsumer));
-    }
-}
+    };
+    return ContextRender;
+}(React.PureComponent));
 exports.ContextRender = ContextRender;
--- dist/src/index.test.js
+++ dist/src/index.test.js
@@ -1,4 +1,28 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -10,83 +34,93 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const React = __importStar(require("react"));
-const react_test_renderer_1 = __importDefault(require("react-test-renderer"));
-const index_1 = require("./index");
-describe('TEST', () => {
-    const appContextValue = {
+var React = __importStar(require("react"));
+var react_test_renderer_1 = __importDefault(require("react-test-renderer"));
+var index_1 = require("./index");
+describe('TEST', function () {
+    var appContextValue = {
         foo: 1,
         bar: 2
     };
-    const appContext = React.createContext(appContextValue);
+    var appContext = React.createContext(appContextValue);
     // #Foo Component
-    const renderFoo = jest.fn(() => null);
-    class FooComponent extends React.Component {
-        render() {
+    var renderFoo = jest.fn(function () { return null; });
+    var FooComponent = /** @class */ (function (_super) {
+        __extends(FooComponent, _super);
+        function FooComponent() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        FooComponent.prototype.render = function () {
             return renderFoo(this.props);
+        };
+        return FooComponent;
+    }(React.Component));
+    var Foo = index_1.withContext('foo')(FooComponent);
+    var renderBar = jest.fn(function () { return null; });
+    var BarComponent = /** @class */ (function (_super) {
+        __extends(BarComponent, _super);
+        function BarComponent() {
+            return _super !== null && _super.apply(this, arguments) || this;
         }
-    }
-    const Foo = index_1.withContext('foo')(FooComponent);
-    const renderBar = jest.fn(() => null);
-    class BarComponent extends React.Component {
-        render() {
+        BarComponent.prototype.render = function () {
             return renderBar(this.props);
-        }
-    }
-    const Bar = index_1.withContext('bar')(BarComponent);
-    const contextRender = jest.fn(() => null);
-    const AppRenderer = react_test_renderer_1.default.create(React.createElement(index_1.ContextCreator, { context: appContext, initContextValue: appContextValue },
+        };
+        return BarComponent;
+    }(React.Component));
+    var Bar = index_1.withContext('bar')(BarComponent);
+    var contextRender = jest.fn(function () { return null; });
+    var AppRenderer = react_test_renderer_1.default.create(React.createElement(index_1.ContextCreator, { context: appContext, initContextValue: appContextValue },
         React.createElement(Foo, null),
         React.createElement(Bar, null),
-        React.createElement(index_1.ContextRender, { keys: ['foo'] }, (props) => contextRender(props))));
-    const AppRendererInstance = AppRenderer.getInstance();
-    const ProviderTestInstance = AppRenderer.root.findByType(index_1.Provider);
-    const providerElement = ProviderTestInstance.instance;
-    const providerDefaultProps = {
+        React.createElement(index_1.ContextRender, { keys: ['foo'] }, function (props) { return contextRender(props); })));
+    var AppRendererInstance = AppRenderer.getInstance();
+    var ProviderTestInstance = AppRenderer.root.findByType(index_1.Provider);
+    var providerElement = ProviderTestInstance.instance;
+    var providerDefaultProps = {
         setContext: providerElement.state.setContext,
         getContext: providerElement.state.getContext
     };
-    it('should render without errors', () => {
+    it('should render without errors', function () {
         expect(AppRendererInstance).toBeTruthy();
     });
-    it('should Foo and Bar rendered with initial context', () => {
-        const initialFooProps = Object.assign({ foo: appContextValue.foo }, providerDefaultProps);
+    it('should Foo and Bar rendered with initial context', function () {
+        var initialFooProps = __assign({ foo: appContextValue.foo }, providerDefaultProps);
         expect(renderFoo).toBeCalledWith(initialFooProps);
-        const initialBarProps = Object.assign({ bar: appContextValue.bar }, providerDefaultProps);
+        var initialBarProps = __assign({ bar: appContextValue.bar }, providerDefaultProps);
         expect(renderBar).toBeCalledWith(initialBarProps);
     });
-    it('should get all initial context', () => {
-        const initialContext = providerDefaultProps.getContext('bar', 'foo');
+    it('should get all initial context', function () {
+        var initialContext = providerDefaultProps.getContext('bar', 'foo');
         expect(initialContext).toEqual(appContextValue);
     });
-    it('should ContextRender rendered with init context and setContext parms', () => {
-        expect(contextRender).toBeCalledWith(Object.assign({ foo: appContextValue.foo }, providerDefaultProps));
+    it('should ContextRender rendered with init context and setContext parms', function () {
+        expect(contextRender).toBeCalledWith(__assign({ foo: appContextValue.foo }, providerDefaultProps));
     });
-    it('should "Foo" and "contextRender" re-render went foo value changed', () => {
+    it('should "Foo" and "contextRender" re-render went foo value changed', function () {
         jest.clearAllMocks();
         providerElement.state.setContext({ foo: 2 });
-        const nextFooValue = 2;
-        const changedTextComponentProps = Object.assign({ foo: nextFooValue }, providerDefaultProps);
+        var nextFooValue = 2;
+        var changedTextComponentProps = __assign({ foo: nextFooValue }, providerDefaultProps);
         expect(renderFoo).toBeCalledWith(changedTextComponentProps);
         expect(contextRender).toBeCalledWith(changedTextComponentProps);
     });
-    it('should "Bar" not re-render went "num" context changed', () => {
+    it('should "Bar" not re-render went "num" context changed', function () {
         providerElement.state.setContext({ num: 2 });
         expect(renderBar).not.toBeCalled();
     });
-    it('should get all changed context', () => {
-        const initialContext = providerDefaultProps.getContext('foo');
+    it('should get all changed context', function () {
+        var initialContext = providerDefaultProps.getContext('foo');
         expect(initialContext).toEqual({
             foo: 2
         });
     });
-    it('should Foo re-render went it own props changed', () => {
+    it('should Foo re-render went it own props changed', function () {
         jest.clearAllMocks();
-        const nextFooPrimayProps = 'new-passed-props';
+        var nextFooPrimayProps = 'new-passed-props';
         AppRenderer.update(React.createElement(index_1.ContextCreator, { context: appContext, initContextValue: appContextValue },
             React.createElement(Foo, { fooPrimayProps: nextFooPrimayProps }),
             React.createElement(Bar, null)));
-        const changedTextComponentProps = Object.assign({ fooPrimayProps: nextFooPrimayProps, foo: 2 }, providerDefaultProps);
+        var changedTextComponentProps = __assign({ fooPrimayProps: nextFooPrimayProps, foo: 2 }, providerDefaultProps);
         expect(renderFoo).toBeCalledWith(changedTextComponentProps);
     });
 });
